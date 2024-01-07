bc8f8e471be0165502a11b5660fa9432db3bdad1
geeksict <153695589+geeksict@users.noreply.github.com>
2024-01-07 12:50:08 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# Set Hostname
-read -p 'Input username defautl: ' unamedf
-passwd -d $unamedf
+# Remove pass for user default
+read -p 'Remove passwd User default (Y/n): ' choicermpasswd
+choice=${choice:-yes}
+if [$choicermpasswd=='Y'] || [$choicermpasswd=='y'] || [$choicermpasswd=='yes']; then
+  read -p 'Input username default: ' unamedf
+  passwd -d $unamedf
+else
+fi
 # Update ubuntu
-apt-get update -y
-apt update -y
-apt upgrade -y
-apt update
+apt-get update -y && apt update -y && apt upgrade -y
+apt install --only-upgrade `apt list --upgradeable 5>/dev/null | cut -d/ -f1 | grep -v Listing`
