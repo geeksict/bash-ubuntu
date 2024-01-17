4f9aabf977d90f55ad7f5db4d2c6c8b7ee96b486
geeksict <153695589+geeksict@users.noreply.github.com>
2024-01-17 10:21:06 +0700

    Update script-ssl.sh

--- script-ssl.sh
+++ script-ssl.sh
@@ -7,16 +7,21 @@ mv /etc/ssl/certs/iRedMail.crt{,.bak}
 mv /etc/ssl/private/iRedMail.key{,.bak}
 ln -s /etc/letsencrypt/live/`hostname -f`/fullchain.pem /etc/ssl/certs/iRedMail.crt
 ln -s /etc/letsencrypt/live/`hostname -f`/privkey.pem /etc/ssl/private/iRedMail.key
+echo "config finished SSL Let's Encrypt for iRedMail"
+
+#fix SSL header
+postconf -e smtpd_tls_security_level=encrypt
+echo -e "\n#\n# fix SSL header\n#"
+postconf -e smtp_tls_cert_file=/etc/ssl/certs/iRedMail.crt
+postconf -e smtp_tls_key_file=/etc/ssl/private/iRedMail.key
+echo "Fix finished SSL Let's Encrypt for header email"
 systemctl restart nginx
 systemctl restart postfix
 systemctl restart dovecot
-
-# Fix SSL header
-sed -i 's/smtpd_tls_security_level = may/smtpd_tls_security_level = encrypt/' /etc/postfix/main.cf
-echo -e '\n#\n# fix SSL header\n#\nsmtp_tls_cert_file = /etc/ssl/certs/iRedMail.crt\nsmtp_tls_key_file = /etc/ssl/private/iRedMail.key' >> /etc/postfix/main.cf
-systemctl restart postfix
+echo "Restarted nginx postfix dovecot"
 
 # Crontab auto renew SSL Let's Encrypt
 (crontab -l ; echo "") | crontab -
 (crontab -l ; echo "# SSL Let's Encrypt auto renew") | crontab -
 (crontab -l ; echo "1   3   *   *   *   certbot renew --post-hook '/usr/sbin/service postfix restart; /usr/sbin/service nginx restart; /usr/sbin/service dovecot restart'") | crontab -
+echo "Config finished CronTab for auto renew SSL # SSL Let's Encrypt"
